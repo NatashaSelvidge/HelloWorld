e04c068e9ef9b64df4a7dd9d0682d6304b1f4441
SAC-CS112-Selvidge-Natasha <natasha.selvidge@gmail.com>
2016-04-13 20:09:20 -0700

    Added files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,17 @@
+
+public class RandomNumber {
+
+	public static void main(String[] args) {
+
+
+		private int num=0;
+		public RandomNumber ();
+
+	Public int GetANumber_Between_1_and_10 ();
+	
+
+	 computer =1+(int)(Math.random()*10);
+
+	  return num;
+	}
+}
