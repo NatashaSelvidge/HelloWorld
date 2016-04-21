b44691ccbbb7ab3e02d1142d3850ddcbe220c1e3
SAC-CS112-Selvidge-Natasha <natasha.selvidge@gmail.com>
2016-04-20 20:41:32 -0700

    Update GuessGame2.java

--- GuessGame2.java
+++ GuessGame2.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
 public class GuessGame2 {
 
 public static void main(String[] args) {
-
+  //create object//
+  RandomNumber randomNumber1 = new RandomNumber
+    "RandomNumber (int high)"
+  RandomNumber randomNumber2 = new RandomNumber
+    "RandomNumber (int low"
 Scanner input = new Scanner (System.in);
 
-double randomNum = 1+(Math.random()*10);
-double number1;
+  int myRandomNum;
+  int numberGuess;
+  int lowNumber;
+  int highNumber;
+  RandomNumber randomNumber = new RandomNumber 
+
 
 System.out.print("Please enter a number between 1 and 10: ");
 number1= input.nextDouble();
